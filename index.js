e1a44ab12a02786479eea75fb1b69d48b25e10c7
Francisco Araujo Ruiz <franxaraujo@gmail.com>
2022-03-15 22:17:25 -0300

    definidas las rutas

--- index.js
+++ index.js
@@ -14,8 +14,25 @@ const routerProductos = express.Router();
 routerProductos.use(express.urlencoded({ extended: true }));
 routerProductos.use(express.json());
 
+let productos = [
+  {
+    Id: 1,
+    title: 'PC',
+    price: 1250,
+    thumbnail:
+      'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcRpDp8UndVwMQ2BH97xSmdNxv7HAp5VCqyBphqq-OpVswSMHnhCz3f7LUl1e9_EGFloBo_QG0noF4w&usqp=CAc',
+  },
+];
+
+const nextID = () => {
+  const ids = productos.map((e) => e.Id);
+  const maxID = ids.length > 0 ? Math.max(...ids) + 1 : 1;
+  //const maxID = productos.length > 0 ? productos.length + 1 : 1;
+  return maxID;
+};
+
 routerProductos.get('/productos', (req, res) => {
-  console.log('get productos');
+  res.json(productos);
 });
 
 routerProductos.get('/productos/:id', (req, res) => {
@@ -23,7 +40,10 @@ routerProductos.get('/productos/:id', (req, res) => {
 });
 
 routerProductos.post('/productos', (req, res) => {
-  console.log('post productos');
+  const newID = nextID();
+  const newProduct = { Id: newID, ...req.body };
+  productos.push(newProduct);
+  res.json({ mensaje: `se agrego producto ${newID} correctamente` });
 });
 
 routerProductos.put('/productos/:id', (req, res) => {
@@ -31,7 +51,13 @@ routerProductos.put('/productos/:id', (req, res) => {
 });
 
 routerProductos.delete('/productos/:id', (req, res) => {
-  console.log('delete productos ID');
+  const productosTemp = productos.filter((e) => e.Id != req.params.id);
+  if (productosTemp.length === productos.length) {
+    res.json({ mensaje: `Error producto Id: ${req.params.id} no encontrado` });
+  } else {
+    productos = productosTemp;
+    res.json({ mensaje: `se eliminó producto ${req.params.id} correctamente` });
+  }
 });
 
 app.use('/api', routerProductos);
